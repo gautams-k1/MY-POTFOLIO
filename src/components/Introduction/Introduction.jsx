daa5ab792acf682f056ced8086237e37a447507d
gautams-k1 <bannabanna808@gmail.com>
2023-10-20 13:46:16 +0530

    Fix bug

--- src/components/Introduction/Introduction.jsx
+++ src/components/Introduction/Introduction.jsx
@@ -4,6 +4,7 @@ import styles from './Introduction.module.css';
 export default function Introduction() {
   return (
     <div className={styles.Introduction} id='Home'>
+      <img className={styles.Introduction_img} src="/assets/header-bg.jpg" alt="img" />
       <div className={styles.Introduction_text}>
             <span className={styles.hello}>Hello!</span>
             <h3><span className={styles.captal}>i'm </span>Gautam singh</h3>
